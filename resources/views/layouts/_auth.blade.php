c4927aaaa791107924275b5aec1b0f94fe42e6d5
mazfaa <kangazfa@gmail.com>
2022-02-25 00:55:14 +0700

    Merapihkan kode

--- resources/views/layouts/_auth.blade.php
+++ resources/views/layouts/_auth.blade.php
@@ -4,25 +4,21 @@
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 <title>The Mentor {{ $auth ?? '' }}</title>
 </head>
 <body>
-
 <div class="container mt-5 mb-5">
-    <div class="row">
-        <div class="col-md-8 mx-auto">
-            <div class="card">
-                <div class="card-header">{{ $card_header ?? '' }}</div>
-                <div class="card-body">{{ $card_body ?? '' }}</div>
-            </div>
-        </div>
+  <div class="row">
+    <div class="col-md-8 mx-auto">
+      <div class="card">
+        <div class="card-header">{{ $card_header ?? '' }}</div>
+        <div class="card-body">{{ $card_body ?? '' }}</div>
+      </div>
     </div>
+  </div>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/layouts/_master.blade.php
+++ resources/views/layouts/_master.blade.php
@@ -1,199 +1,184 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>The Mentor's Laundry</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- Bootstrap -->
-    <link href="{{ asset('assets') }}/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="{{ asset('assets') }}/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{ asset('assets') }}/vendors/nprogress/nprogress.css" rel="stylesheet">
-
-    <!-- Custom Theme Style -->
-    <link href="{{ asset('assets') }}/build/css/custom.min.css" rel="stylesheet">
-    <!-- Datatables -->
-    <link rel="stylesheet" href="//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
-    <!-- Bootstrap Icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- Meta, title, CSS, favicons, etc. -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>The Mentor's Laundry</title>
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- Bootstrap -->
+<link href="{{ asset('assets') }}/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="{{ asset('assets') }}/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- NProgress -->
+<link href="{{ asset('assets') }}/vendors/nprogress/nprogress.css" rel="stylesheet">
+<!-- Custom Theme Style -->
+<link href="{{ asset('assets') }}/build/css/custom.min.css" rel="stylesheet">
+<!-- Datatables -->
+<link rel="stylesheet" href="//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
+<!-- Bootstrap Icon -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </head>
-
 <body class="nav-md">
-    <div class="container body">
-        <div class="main_container">
-            <div class="col-md-3 left_col">
-                <div class="left_col scroll-view">
-                    <div class="navbar nav_title" style="border: 0;">
-                        <a href="/" class="site_title"><span><i class="fa fa-paw"></i> Mentor's Laundry</span></a>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <!-- menu profile quick info -->
-                    <div class="profile clearfix">
-                        <div class="profile_pic">
-                            @if (Auth::user()->gender === 'L' && Auth::user()->username !== 'mazfaa')
-                                <img src="{{ asset('assets') }}/images/man_avatar.png" alt="user-logged-in" class="img-circle profile_img">
-                            @elseif (Auth::user()->gender === 'P')
-                                <img src="{{ asset('assets') }}/images/akhwat_avatar.png" alt="user-logged-in" class="img-circle profile_img">
-                            @elseif (Auth::user()->username === 'mazfaa')
-                                <img src="{{ asset('assets') }}/images/azfa.jpg" alt="user-logged-in" class="img-circle profile_img">
-                            @endif
-                        </div>
-                        <div class="profile_info">
-                            <span>Welcome,</span>
-                            <h2>
-                                {{ Auth::user()->name }}
-                            </h2>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <!-- /menu profile quick info -->
-                    <br />
-                    <!-- sidebar menu -->
-                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                        <div class="menu_section">
-                            <ul class="nav side-menu">
-                                <li><a>Dashboard <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        @if (Auth::user()->role === 'admin')
-                                            @include('layouts._admin')
-                                        @elseif (Auth::user()->role === 'cashier')
-                                            @include('layouts._cashier')
-                                        @elseif (Auth::user()->role === 'owner')
-                                            @include('layouts._owner')
-                                        @endif
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /sidebar menu -->
-                </div>
+<div class="container body">
+  <div class="main_container">
+    <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+                <a href="/" class="site_title"><span><i class="fa fa-paw"></i> Mentor's Laundry</span></a>
             </div>
-
-            <!-- top navigation -->
-            <div class="top_nav">
-                <div class="nav_menu">
-                    <div class="nav toggle">
-                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                    </div>
-                    <nav class="nav navbar-nav">
-                        <ul class=" navbar-right">
-                            <li class="nav-item dropdown open" style="padding-left: 15px;">
-                                <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true"
-                                    id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                                    @if (Auth::user()->gender === 'L' && Auth::user()->username !== 'mazfaa')
-                                        <img src="{{ asset('assets') }}/images/man_avatar.png" alt="user-logged-in"> {{ Auth::user()->name }}
-                                    @elseif (Auth::user()->gender === 'P')
-                                        <img src="{{ asset('assets') }}/images/akhwat_avatar.png" alt="user-logged-in"> {{ Auth::user()->name }}
-                                    @elseif (Auth::user()->username === 'mazfaa')
-                                        <img src="{{ asset('assets') }}/images/azfa.jpg" alt="user-logged-in"> {{ Auth::user()->name }}
-                                    @endif
-                                </a>
-                                <div class="dropdown-menu dropdown-usermenu pull-right"
-                                    aria-labelledby="navbarDropdown">
-                                    <form action="{{ route('logout') }}" method="post">
-                                        @csrf
-                                        <button type="submit" class="btn text-center">
-                                            <i class="fa fa-sign-out pull-right"></i> Logout
-                                        </button>
-                                    </form>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
+            <div class="clearfix"></div>
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+                <div class="profile_pic">
+                    @if (Auth::user()->gender === 'L' && Auth::user()->username !== 'mazfaa')
+                        <img src="{{ asset('assets') }}/images/man_avatar.png" alt="user-logged-in" class="img-circle profile_img">
+                    @elseif (Auth::user()->gender === 'P')
+                        <img src="{{ asset('assets') }}/images/akhwat_avatar.png" alt="user-logged-in" class="img-circle profile_img">
+                    @elseif (Auth::user()->username === 'mazfaa')
+                        <img src="{{ asset('assets') }}/images/azfa.jpg" alt="user-logged-in" class="img-circle profile_img">
+                    @endif
                 </div>
-            </div>
-            <!-- /top navigation -->
-
-            <!-- page content -->
-            <div class="right_col" role="main">
-                <div class="">
-                    <div class="page-title">
-                        <div class="title_left">
-                        </div>
-
-                        <div class="title_right">
-                            <div class="col-md-5 col-sm-5   form-group pull-right top_search">
-                                <div class="input-group">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="row">
-                        <div class="col-md-12 col-sm-12  ">
-                            {{ $status ?? '' }}
-                            <div class="x_panel">
-                                <div class="x_title">
-                                    <h2>{{ $header_page ?? '' }}</h2>
-                                    <ul class="nav navbar-right panel_toolbox">
-                                        {{ $header_btn ?? '' }}
-                                    </ul>
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="x_content">
-                                    {{ $content_page ?? '' }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /page content -->
-
-            <!-- footer content -->
-            <footer>
-                <div class="pull-right">
-                    Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+                <div class="profile_info">
+                    <span>Welcome,</span>
+                    <h2>
+                      {{ Auth::user()->name }}
+                    </h2>
                 </div>
                 <div class="clearfix"></div>
-            </footer>
-            <!-- /footer content -->
+            </div>
+            <!-- /menu profile quick info -->
+            <br />
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                <div class="menu_section">
+                    <ul class="nav side-menu">
+                        <li><a>Dashboard <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                @if (Auth::user()->role === 'admin')
+                                  @include('layouts._admin')
+                                @elseif (Auth::user()->role === 'cashier')
+                                  @include('layouts._cashier')
+                                @elseif (Auth::user()->role === 'owner')
+                                  @include('layouts._owner')
+                                @endif
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /sidebar menu -->
         </div>
     </div>
 
-    <!-- jQuery -->
-    <script src="{{ asset('assets') }}/vendors/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="{{ asset('assets') }}/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-    <!-- FastClick -->
-    <script src="{{ asset('assets') }}/vendors/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="{{ asset('assets') }}/vendors/nprogress/nprogress.js"></script>
+    <!-- top navigation -->
+    <div class="top_nav">
+        <div class="nav_menu">
+            <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+            </div>
+            <nav class="nav navbar-nav">
+                <ul class=" navbar-right">
+                    <li class="nav-item dropdown open" style="padding-left: 15px;">
+                        <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true"
+                            id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                            @if (Auth::user()->gender === 'L' && Auth::user()->username !== 'mazfaa')
+                              <img src="{{ asset('assets') }}/images/man_avatar.png" alt="user-logged-in"> {{ Auth::user()->name }}
+                            @elseif (Auth::user()->gender === 'P')
+                              <img src="{{ asset('assets') }}/images/akhwat_avatar.png" alt="user-logged-in"> {{ Auth::user()->name }}
+                            @elseif (Auth::user()->username === 'mazfaa')
+                              <img src="{{ asset('assets') }}/images/azfa.jpg" alt="user-logged-in"> {{ Auth::user()->name }}
+                            @endif
+                        </a>
+                        <div class="dropdown-menu dropdown-usermenu pull-right"
+                            aria-labelledby="navbarDropdown">
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn text-center">
+                                    <i class="fa fa-sign-out pull-right"></i> Logout
+                                </button>
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!-- /top navigation -->
 
-    <!-- Custom Theme Scripts -->
-    <script src="{{ asset('assets') }}/build/js/custom.min.js"></script>
-    <!-- Datatables -->
-    <script src="//cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-    <!-- Script js -->
-    <script src="{{ asset('assets/build/js/script.js') }}"></script>
-    <script>
-        $(document).ready(function() {
-            $('#outlet-table').DataTable();
-            $('#package-table').DataTable();
-            $('#member-table').DataTable();
-            $('#employee-table').DataTable();
-            $('#inventory-table').DataTable();
-        });
-    </script>
+    <!-- page content -->
+    <div class="right_col" role="main">
+      <div class="">
+        <div class="page-title">
+          <div class="title_left">
+          </div>
+          <div class="title_right">
+            <div class="col-md-5 col-sm-5 form-group pull-right top_search">
+              <div class="input-group">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+          <div class="col-md-12 col-sm-12  ">
+            {{ $status ?? '' }}
+            <div class="x_panel">
+              <div class="x_title">
+                <h2>{{ $header_page ?? '' }}</h2>
+                <ul class="nav navbar-right panel_toolbox">
+                  {{ $header_btn ?? '' }}
+                </ul>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                {{ $content_page ?? '' }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- /page content -->
+
+  <!-- footer content -->
+  <footer>
+    <div class="pull-right">
+      Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+    </div>
+    <div class="clearfix"></div>
+  </footer>
+  <!-- /footer content -->
+  </div>
+</div>
+<!-- jQuery -->
+<script src="{{ asset('assets') }}/vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('assets') }}/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
+<!-- FastClick -->
+<script src="{{ asset('assets') }}/vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="{{ asset('assets') }}/vendors/nprogress/nprogress.js"></script>
+<!-- Custom Theme Scripts -->
+<script src="{{ asset('assets') }}/build/js/custom.min.js"></script>
+<!-- Datatables -->
+<script src="//cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+<!-- Script js -->
+<script src="{{ asset('assets/build/js/script.js') }}"></script>
+<script>
+  $(document).ready(function() {
+    $('#outlet-table').DataTable();
+    $('#package-table').DataTable();
+    $('#member-table').DataTable();
+    $('#employee-table').DataTable();
+    $('#inventory-table').DataTable();
+  });
+</script>
 </body>
-
 </html>
